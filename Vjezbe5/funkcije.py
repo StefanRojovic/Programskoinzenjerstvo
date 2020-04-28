7d7b33b18de14b752686f8bdd5c55a01b123013c
StefanRojovic <62648700+StefanRojovic@users.noreply.github.com>
2020-04-28 21:00:15 +0200

    Vjezbe5

---
+++ Vjezbe5/funkcije.py
@@ -0,0 +1,26 @@
+import likovi
+import math
+from math import pi
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+    def opseg(parametar):
+        if isinstance(parametar , likovi.Kruznica):
+            return 2 * parametar.radijus * pi
+        elif isinstance(parametar , likovi.Kvadrat):
+            return 4 * parametar.stranica
+
+    def povrsina(parametar):
+        if isinstance(parametar , likovi.Kruznica):
+            return math.pow(parametar.radijus , 2) * pi
+        elif isinstance(parametar , likovi.Kvadrat):
+            return 4 * parametar.stranica
+
+    if __name__ == '__main__':
+        print('*** test funkcije ***')
+        print(opseg.__name__)
+        print(povrsina.__name__)
