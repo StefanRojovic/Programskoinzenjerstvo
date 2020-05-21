5d57e132804e694dea23dca787329eb46d729a22
StefanRojovic <62648700+StefanRojovic@users.noreply.github.com>
2020-05-21 21:39:08 +0200

    Vjezba 8

---
+++ Vjezba 8/slagalica.py
@@ -0,0 +1,48 @@
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+    class Polje(object):
+
+     def __init__(self, broj = 0):
+          self.__broj = broj
+
+     @property
+     def vratiBroj(self):
+          return self.__broj
+
+     @property
+     def jeBroj(self):
+          if self.__broj != 0:
+               return True
+          else:
+               return False
+
+     @property
+     def jePrazno(self):
+          if self.__broj == 0:
+               return True
+          else:
+               return False
+
+     def __str__(self):
+          if self.jeBroj == True:
+               return '%d' % self.__broj
+          if self.jePrazno == True:
+               return ' '
+
+     def __repr__(self):
+          return 'Polje(%s)' % (self.__broj)
+
+print('*** test 1 ***')
+brojevi = list(range(9))
+for broj in brojevi:
+     p = Polje(broj)
+     print(p.vratiBroj, p.jeBroj, p.jePrazno)
+
+print('*** test 2 ***')
+polja = [Polje(broj) for broj in range(9)]
+for p in polja:
+     print(repr(str(p)), repr(p))
